1da90ed4d81a5e3df4c525c893ed0499e01dff42
Nisal Jayarathna <it25102772@my.sliit.lk>
2026-09-02 15:18:44 +0530

    config: intialize Spring Boot API

---
+++ api/src/main/java/com/jayarathna/powertools/PowertoolsApplication.java
@@ -0,0 +1,13 @@
+package com.jayarathna.powertools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowertoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowertoolsApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/jayarathna/powertools/PowertoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayarathna.powertools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowertoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
