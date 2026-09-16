e7d7dee25e389e315cdc5e24cf44da57380529ee
Nisal Jayarathna <it25102772@my.sliit.lk>
2026-09-16 15:28:51 +0530

    fix(api): add missing GenerationType import in Category entity

--- api/src/main/java/com/jayarathna/powertools/feature/product/Category.java
+++ api/src/main/java/com/jayarathna/powertools/feature/product/Category.java
@@ -2,6 +2,7 @@ package com.jayarathna.powertools.feature.product;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
